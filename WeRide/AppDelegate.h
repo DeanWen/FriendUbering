e4ef9bf7554656a71173919df67315f638a4f983
dian.wen <2010wendian@gmail.com>
2015-07-24 15:38:01 -0400

    Change to Tabe View

---
+++ WeRide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeRide
+//
+//  Created by Dian Wen on 7/24/15.
+//  Copyright (c) 2015 Dian Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeRide/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  WeRide
+//
+//  Created by Dian Wen on 7/24/15.
+//  Copyright (c) 2015 Dian Wen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
